c92390e3fcf6bc2324afbdf21eea5cf202d76cb0
Marco Antonio Azevedo de Araujo Arcoverde <125325138+marcoarc01@users.noreply.github.com>
2024-04-28 20:44:19 -0300

    Update Cliente.java

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -3,40 +3,60 @@ package entities;
 import java.util.ArrayList;
 
 public class Cliente extends Pessoa{
-	private Integer idCliente;
-	protected ArrayList<Livro> livrosEmprestados;
-	
-	public Cliente() {
-	}
+    protected String usuario;
+    protected String senha;
+    private Integer idCliente;
+    protected ArrayList<Livro> livrosEmprestados;
 
-	public Cliente(String nome, String endereco, String telefone, Integer idCliente,
-			ArrayList<Livro> livrosEmprestados) {
-		super(nome, endereco, telefone);
-		this.idCliente = idCliente;
-		this.livrosEmprestados = livrosEmprestados;
-	}
+    public Cliente() {
+    }
+    public Cliente (String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    public Cliente(String nome, String endereco, String telefone, Integer idCliente,
+                   ArrayList<Livro> livrosEmprestados) {
+        super(nome, endereco, telefone);
+        this.idCliente = idCliente;
+        this.livrosEmprestados = livrosEmprestados;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
 
-	public Integer getIdCliente() {
-		return idCliente;
-	}
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 
-	public void setIdCliente(Integer idCliente) {
-		this.idCliente = idCliente;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public ArrayList<Livro> getLivrosEmprestados() {
-		return livrosEmprestados;
-	}
-	
-	public void emprestarLivro(Livro livro) {
-		livrosEmprestados.add(livro);
-	}
-	
-	public void devolverLivro(Livro livro) {
-		livrosEmprestados.remove(livro);
-	}
-	
-	public void imprimirDetalhes() {
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public ArrayList<Livro> getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public void emprestarLivro(Livro livro) {
+        livrosEmprestados.add(livro);
+    }
+
+    public void devolverLivro(Livro livro) {
+        livrosEmprestados.remove(livro);
+    }
+
+    public void imprimirDetalhes() {
         super.imprimirDetalhes();
         System.out.println("ID do Cliente: " + idCliente);
         System.out.println("Livros Emprestados:");
