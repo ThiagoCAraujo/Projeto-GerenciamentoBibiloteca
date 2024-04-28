44a82d222cd30bf8ee09072625e9087f4738441e
pedrometal <163933671+pedrometal@users.noreply.github.com>
2024-04-28 11:07:43 -0300

    Update Livro.java

--- src/application/main-pedro/Livro.java
+++ src/application/main-pedro/Livro.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Criando instâncias de livros
+        // criando os livros
         Livro livro1 = new Livro("Os Códigos do Milhão", "Pablo Marçal", 1, true);
         Livro livro2 = new Livro("O homem mais rico da Babilônia", "George Samuel Clason", 2, false);
         Livro livro3 = new Livro("Do Mil ao Milhão. Sem Cortar o Cafezinho", "Thiago Nigro", 3, true);
         Livro livro4 = new Livro("A arte da negociação", "Donald J. Trump", 4, false);
 
-        // Exibindo informações dos livros
+        // info dos livros
         System.out.println("Informações do Livro 1:");
         exibirInformacoesLivro(livro1);
 
@@ -19,12 +19,12 @@ public class Main {
         System.out.println("\nInformações do Livro 4:");
         exibirInformacoesLivro(livro4);
 
-        // Alterando o status do Livro 2 para disponível
+        // botando livro 2 para disponivel
         livro2.setStatus(true);
         System.out.println("\nStatus do Livro 2 atualizado para disponível.");
         System.out.println("Novo status do Livro 2: " + (livro2.getStatus() ? "Disponível" : "Indisponível"));
     }
-
+     \\ botei essa função pra ficar mais facil de adicionar
     public static void exibirInformacoesLivro(Livro livro) {
         System.out.println("Título: " + livro.getTitulo());
         System.out.println("Autor: " + livro.getAutor());
