bc653061045723ec1431b5131ce42948b005737e
Marco Antonio Azevedo de Araujo Arcoverde <125325138+marcoarc01@users.noreply.github.com>
2024-04-28 20:43:51 -0300

    Update Conta.java

--- src/entities/Conta.java
+++ src/entities/Conta.java
@@ -1,28 +1,28 @@
 package entities;
 
-public class Conta {
-    private Usuario[] usuarios;
+public class Conta extends Cliente{
+    private Cliente[] usuarios;
 
     public Conta() {
-        this.usuarios = new Usuario[0];
+        this.usuarios = new Cliente[0];
     }
 
     public boolean adicionarUsuario(String usuario, String senha) {
-        for (Usuario user : usuarios) {
+        for (Cliente user : usuarios) {
             if (user.getUsuario().equals(usuario)) {
                 return false;
             }
         }
-        Usuario novoUsuario = new Usuario(usuario, senha);
-        Usuario[] temp = new Usuario[usuarios.length + 1];
+        Cliente novoUsuario = new Cliente(usuario, senha);
+        Cliente[] temp = new Cliente[usuarios.length + 1];
         System.arraycopy(usuarios, 0, temp, 0, usuarios.length);
         temp[temp.length - 1] = novoUsuario;
         usuarios = temp;
         return true;
     }
 
-    public Usuario fazerLogin(String usuario, String senha) {
-        for (Usuario user : usuarios) {
+    public Cliente fazerLogin(String usuario, String senha) {
+        for (Cliente user : usuarios) {
             if (user.getUsuario().equals(usuario) && user.getSenha().equals(senha)) {
                 return user;
             }
