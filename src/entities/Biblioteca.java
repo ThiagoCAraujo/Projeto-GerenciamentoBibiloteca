f48a6a0f7d02a3befc72144d099d03e8d9b5869e
ThiagoCAraujo <133007228+ThiagoCAraujo@users.noreply.github.com>
2024-04-28 21:00:59 -0300

    Update Biblioteca.java

--- src/entities/Biblioteca.java
+++ src/entities/Biblioteca.java
@@ -3,55 +3,56 @@ package entities;
 import java.util.ArrayList;
 
 public class Biblioteca {
-    protected ArrayList<Livro> listaLivros;
-    protected ArrayList<Cliente> listaClientes;
-    protected ArrayList<String> livrosDisponiveis;
-
-
-    public Biblioteca() {
-    }
-
-    public Biblioteca(ArrayList<Livro> listaLivros, ArrayList<Cliente> listaClientes, ArrayList<String> livrosDisponiveis) {
-        this.listaLivros = listaLivros;
-        this.listaClientes = listaClientes;
-        this.livrosDisponiveis = livrosDisponiveis;
-    }
-
-    public void adicionarLivro(Livro livro) {
-        listaLivros.add(livro);
-    }
-
-    public void removerLivro(Livro livro) {
-        listaLivros.remove(livro);
-    }
-
-    public void adicionarCliente(Cliente cliente) {
-        listaClientes.add(cliente);
-    }
-
-    public void removerCliente(Cliente cliente) {
-        listaClientes.remove(cliente);
-    }
-
-    public void emprestarLivro(Cliente cliente, Livro livro) {
-        if(livro.getStatus()) {
-            livro.setStatus(false);
-            cliente.emprestarLivro(livro);
-        } else {
-            System.out.println("Livro nao disponivel para emprestimo!");
-        }
-    }
-
-    public void devolverLivro(Cliente cliente, Livro livro) {
-        if(livro.getStatus()) {
-            cliente.devolverLivro(livro);
-            livro.setStatus(true);
-        } else {
-            System.out.println("Esse livro nao foi emprestado por esse cliente!");
-        }
-    }
-
-    public ArrayList<String> livrosDisponiveis() {
+	protected ArrayList<Livro> listaLivros;
+	protected ArrayList<Cliente> listaClientes;
+	protected ArrayList<String> livrosDisponiveis;
+	
+	
+	public Biblioteca() {
+	}
+	
+	public Biblioteca(ArrayList<Livro> listaLivros, ArrayList<Cliente> listaClientes, 
+			ArrayList<String> livrosDisponiveis) {
+		this.listaLivros = listaLivros;
+		this.listaClientes = listaClientes;
+		this.livrosDisponiveis = livrosDisponiveis;
+	}
+	
+	public void adicionarLivro(Livro livro) {
+		listaLivros.add(livro);
+	}
+	
+	public void removerLivro(Livro livro) {
+		listaLivros.remove(livro);
+	}
+	
+	public void adicionarCliente(Cliente cliente) {
+		listaClientes.add(cliente);
+	}
+	
+	public void removerCliente(Cliente cliente) {
+		listaClientes.remove(cliente);
+	}
+	
+	public void emprestarLivro(Cliente cliente, Livro livro) {
+		if(livro.getStatus()) {
+			livro.setStatus(false);
+			cliente.emprestarLivro(livro);
+		} else {
+			System.out.println("Livro nao disponivel para emprestimo!");
+		}
+	}
+	
+	public void devolverLivro(Cliente cliente, Livro livro) {
+		if(livro.getStatus()) {
+			cliente.devolverLivro(livro);
+			livro.setStatus(true);
+		} else {
+			System.out.println("Esse livro nao foi emprestado por esse cliente!");
+		}
+	}
+	
+	public ArrayList<String> livrosDisponiveis() {
         livrosDisponiveis.clear();
         for (Livro livro : listaLivros) {
             if (livro.getStatus()) {
