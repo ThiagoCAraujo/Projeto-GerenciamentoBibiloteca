9fde5181c3662db9436016bb73de5313aa741731
ThiagoCAraujo <133007228+ThiagoCAraujo@users.noreply.github.com>
2024-04-29 15:16:16 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,138 +1,202 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import entities.Biblioteca;
 import entities.Cliente;
-import entities.Conta;
 import entities.Livro;
+
 
 public class Main {
     public static void main(String[] args) {
-    	
-    	Livro livro1 = new Livro("Os Códigos do Milhão", "Pablo Marçal", 1, true);
-        Livro livro2 = new Livro("O homem mais rico da Babilônia", "George Samuel Clason", 2, false);
-        Livro livro3 = new Livro("Do Mil ao Milhão. Sem Cortar o Cafezinho", "Thiago Nigro", 3, true);
-        Livro livro4 = new Livro("A arte da negociação", "Donald J. Trump", 4, false);
+        Biblioteca biblioteca = new Biblioteca();
+        inicializarBiblioteca(biblioteca);
 
-        // info dos livros
-        System.out.println("Informações do Livro 1:");
-        livro1.exibirInformacoes();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nInformações do Livro 2:");
-        livro2.exibirInformacoes();
+        System.out.println("BEM-VINDO(A) A BIBLIOTECA DO CARANGUEIJO!!");
+        
+        int opcao;
+        do {
+            System.out.println("\n=== Menu ===");
+            System.out.println("1. Menu Cliente");
+            System.out.println("2. Menu Funcionário");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
 
-        System.out.println("\nInformações do Livro 3:");
-        livro3.exibirInformacoes();
-
-        System.out.println("\nInformações do Livro 4:");
-        livro4.exibirInformacoes();
-
-        // botando livro 2 para disponivel
-        livro2.setStatus(true);
-        System.out.println("\nStatus do Livro 2 atualizado para disponível.");
-        System.out.println("Novo status do Livro 2: " + (livro2.getStatus() ? "Disponível" : "Indisponível"));
-      
-
-    	Scanner scan = new Scanner(System.in);
-    	Conta conta = new Conta();
-    	Biblioteca biblioteca = new Biblioteca();
-
-    	boolean encerrarPrograma = false;
-    	while (!encerrarPrograma) {
-    	System.out.println("BEM VINDO A BIBLIOTECA DO CARANGUEJO\n");
-        System.out.print("\n1- Sou funcionario \n2- Sou cliente \n0- Encerrar programa \nEscolha uma opção: ");
-            int opcao1 = scan.nextInt();
-            switch (opcao1) {
-                case 0:
-                    encerrarPrograma = true;
-                    break;
+            switch (opcao) {
                 case 1:
-                    System.out.println("\n1- Adicionar livro \n2- Remover livro \n3- Adicionar cliente \n4- Remover cliente ");
-                    int opcao3 = scan.nextInt();
-                    switch (opcao3) {
-                        case 1:
-                            System.out.println("Digite o titulo do livro: ");
-                            scan.nextLine();
-                            String titulo = scan.nextLine();
-                            System.out.println("digite o autor do livro: ");
-                            String autor = scan.nextLine();
-                            System.out.println("digite o id do livro: ");
-                            int idLivro = scan.nextInt();
-                            Livro livro = new Livro(titulo, autor, idLivro, false);
-                            biblioteca.adicionarLivro(livro);
-                            break;
-                        case 2:
-                            System.out.println("livros disponiveis:");
-                            ArrayList<String> livrosDisponiveis = biblioteca.livrosDisponiveis();
-                            for (String titulos : livrosDisponiveis) {
-                                System.out.println("- " + titulos);
-                            }
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        default:
-                            System.out.println("opção esta invalida. verifique e tente novamente");
-                    }
+                    menuCliente(biblioteca);
                     break;
-                    
                 case 2:
-                    boolean voltarMenuPrincipal = false;
-                    while (!voltarMenuPrincipal) {
-                        System.out.println("bem vindo a parte do cliente:");
-                        System.out.print("1- login \n2- criar conta \n3- voltar \nescolha uma opção:");
-                        int opcao2 = scan.nextInt();
-                        switch (opcao2) {
-                            case 1:
-                                fazerLogin(scan, conta);
-                                break;
-                            case 2:
-                                criarNovoLogin(scan, conta);
-                                break;
-                            case 3:
-                                voltarMenuPrincipal = true;
-                                break;
-                            default:
-                                System.out.println("opção esta invalida. verifique e tente novamente");
-                        }
-                    }
+                    menuFuncionario(biblioteca);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
                     break;
                 default:
-                    System.out.println("opção esta invalida. verifique e tente novamente");
+                    System.out.println("Opção inválida!");
             }
+        } while (opcao != 0);
+    }
+
+    // Método para inicializar a biblioteca com 4 livros
+    public static void inicializarBiblioteca(Biblioteca biblioteca) {
+        biblioteca.adicionarLivro(new Livro("Os Códigos do Milhão", "Pablo Marçal", 1));
+        biblioteca.adicionarLivro(new Livro("O homem mais rico da Babilônia", "George Samuel Clason", 2));
+        biblioteca.adicionarLivro(new Livro("Do Mil ao Milhão. Sem Cortar o Cafezinho", "Thiago Nigro", 3));
+        biblioteca.adicionarLivro(new Livro("A arte da negociação", "Donald J. Trump", 4));
+    }
+
+    // Método para o menu do cliente
+    public static void menuCliente(Biblioteca biblioteca) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n=== Menu Cliente ===");
+        System.out.println("1. Lista de Livros Disponíveis");
+        System.out.println("2. Emprestar Livro");
+        System.out.println("3. Devolver Livro");
+        System.out.println("0. Voltar");
+
+        System.out.print("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+        	case 1:
+        		System.out.println("\nLivros Disponíveis:");
+                for (Livro livro : biblioteca.getListaLivros()) {
+                    if (livro.getStatus().equals("Disponível")) {
+                        livro.imprimirDetalhes();
+                    }
+                }
+                break;
+             case 2:
+               	System.out.print("Digite o ID do Cliente: ");
+               	int idClienteEmprestimo = scanner.nextInt();
+               	Cliente clienteEmprestimo = biblioteca.encontrarClientePorID(idClienteEmprestimo);
+               	if (clienteEmprestimo != null) {
+               		System.out.print("Digite o ID do Livro que deseja emprestar: ");
+               		int idLivroEmprestimo = scanner.nextInt();
+               		Livro livroEmprestimo = biblioteca.encontrarLivroPorID(idLivroEmprestimo);
+                    if (livroEmprestimo != null) {
+                        biblioteca.emprestarLivro(clienteEmprestimo, livroEmprestimo);
+                    } else {
+                        System.out.println("Livro não encontrado.");
+                    }
+                } else {
+                    System.out.println("Cliente não encontrado.");
+                }
+                break;
+            case 3:
+                System.out.print("Digite o ID do Cliente: ");
+                int idClienteDevolucao = scanner.nextInt();
+                Cliente clienteDevolucao = biblioteca.encontrarClientePorID(idClienteDevolucao);
+                if (clienteDevolucao != null) {
+                    System.out.print("Digite o ID do Livro que deseja devolver: ");
+                    int idLivroDevolucao = scanner.nextInt();
+                    Livro livroDevolucao = biblioteca.encontrarLivroPorID(idLivroDevolucao);
+                    if (livroDevolucao != null) {
+                        biblioteca.devolverLivro(clienteDevolucao, livroDevolucao);
+                    } else {
+                        System.out.println("Livro não encontrado.");
+                    }
+                } else {
+                    System.out.println("Cliente não encontrado.");
+                }
+                break;
+            case 0:
+                System.out.println("Voltando ao menu principal...");
+                break;
+            default:
+                System.out.println("Opção inválida!");
         }
     }
 
-    private static void fazerLogin(Scanner scan, Conta conta) {
-        System.out.print("Usuário: ");
-        scan.nextLine();
-        String usuario = scan.nextLine();
-        System.out.print("Senha: ");
-        String senha = scan.nextLine();
-        Cliente usuarioLogado = conta.fazerLogin(usuario, senha);
-        if (usuarioLogado != null) {
-            exibirOperacoesCliente(scan, usuarioLogado, conta);
-        } else {
-            System.out.println("Usuário ou senha incorretos. Verifique e Tente novamente");
-        }
-    }
+    // Método para o menu do funcionário
+    public static void menuFuncionario(Biblioteca biblioteca) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n=== Menu Funcionário ===");
+        System.out.println("1. Adicionar Livro");
+        System.out.println("2. Remover Livro");
+        System.out.println("3. Adicionar Cliente");
+        System.out.println("4. Remover Cliente");
+        System.out.println("5. Lista de Clientes");
+        System.out.println("6. Lista dos Livros");
+        System.out.println("0. Voltar");
 
-    private static void exibirOperacoesCliente(Scanner scan, Cliente usuarioLogado, Conta conta) {
+        System.out.print("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
 
-    }
-    private static void criarNovoLogin(Scanner scan, Conta conta) {
-        System.out.print("Digite o novo nome de usuário: ");
-        scan.nextLine();
-        String novoUsuario = scan.nextLine();
-        System.out.print("Digite a nova senha: ");
-        String novaSenha = scan.nextLine();
-        if (conta.adicionarUsuario(novoUsuario, novaSenha)) {
-            System.out.println("Sua conta foi criada com sucesso");
-        } else {
-            System.out.println("Esse Usuário já existe. Escolha outro nome de usuário.");
+        switch (opcao) {
+            case 1:
+                // Lógica para adicionar livro
+                System.out.print("Digite o título do livro: ");
+                String tituloLivro = scanner.next();
+                System.out.print("Digite o autor do livro: ");
+                String autorLivro = scanner.next();
+                System.out.print("Digite o ID do livro: ");
+                int idLivro = scanner.nextInt();
+                biblioteca.adicionarLivro(new Livro(tituloLivro, autorLivro, idLivro));
+                System.out.println("Livro adicionado com sucesso!");
+                break;
+            case 2:
+                // Lógica para remover livro
+                System.out.print("Digite o ID do livro que deseja remover: ");
+                int idLivroRemover = scanner.nextInt();
+                Livro livroRemover = biblioteca.encontrarLivroPorID(idLivroRemover);
+                if (livroRemover != null) {
+                    biblioteca.removerLivro(livroRemover);
+                    System.out.println("Livro removido com sucesso!");
+                } else {
+                    System.out.println("Livro não encontrado.");
+                }
+                break;
+            case 3:
+                // Lógica para adicionar cliente
+                System.out.print("Digite o nome do cliente: ");
+                String nomeCliente = scanner.next();
+                System.out.print("Digite o endereço do cliente: ");
+                String enderecoCliente = scanner.next();
+                System.out.print("Digite o telefone do cliente: ");
+                String telefoneCliente = scanner.next();
+                System.out.print("Digite o ID do cliente: ");
+                int idCliente = scanner.nextInt();
+                biblioteca.adicionarCliente(new Cliente(nomeCliente, enderecoCliente, telefoneCliente, idCliente));
+                System.out.println("Cliente adicionado com sucesso!");
+                break;
+            case 4:
+                // Lógica para remover cliente
+                System.out.print("Digite o ID do cliente que deseja remover: ");
+                int idClienteRemover = scanner.nextInt();
+                Cliente clienteRemover = biblioteca.encontrarClientePorID(idClienteRemover);
+                if (clienteRemover != null) {
+                    biblioteca.removerCliente(clienteRemover);
+                    System.out.println("Cliente removido com sucesso!");
+                } else {
+                    System.out.println("Cliente não encontrado.");
+                }
+                break;
+            case 5:
+                // Listar todos os clientes
+                System.out.println("\nLista de Clientes:");
+                for (Cliente cliente : biblioteca.getListaClientes()) {
+                    cliente.imprimirDetalhes();
+                    System.out.println();
+                }
+                break;
+            case 6:
+                // Imprimir todos os livros
+                System.out.println("\nTodos os Livros:");
+                for (Livro livro : biblioteca.getListaLivros()) {
+                    livro.imprimirDetalhes();
+                    System.out.println("Status: " + livro.getStatus());
+                }
+                break;
+            case 0:
+                System.out.println("Voltando ao menu principal...");
+                break;
+            default:
+                System.out.println("Opção inválida!");
         }
     }
 }
