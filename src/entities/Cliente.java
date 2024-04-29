5f92c9b81b92fdeb35a76c0fe3af0c346afc2a57
ThiagoCAraujo <133007228+ThiagoCAraujo@users.noreply.github.com>
2024-04-29 15:18:35 -0300

    Update Cliente.java

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -2,45 +2,23 @@ package entities;
 
 import java.util.ArrayList;
 
-public class Cliente extends Pessoa{
-    protected String usuario;
-    protected String senha;
-    private Integer idCliente;
+public class Cliente extends Pessoa {
+    private int idCliente;
     protected ArrayList<Livro> livrosEmprestados;
 
-    public Cliente() {
-    }
-    public Cliente (String usuario, String senha){
-        this.usuario = usuario;
-        this.senha = senha;
-    }
-    public Cliente(String nome, String endereco, String telefone, Integer idCliente,
-                   ArrayList<Livro> livrosEmprestados) {
+    // Construtor
+    public Cliente(String nome, String endereco, String telefone, int idCliente) {
         super(nome, endereco, telefone);
         this.idCliente = idCliente;
-        this.livrosEmprestados = livrosEmprestados;
-    }
-    public String getUsuario() {
-        return usuario;
+        this.livrosEmprestados = new ArrayList<>();
     }
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Integer getIdCliente() {
+    // Getters e Setters
+    public int getIdCliente() {
         return idCliente;
     }
 
-    public void setIdCliente(Integer idCliente) {
+    public void setIdCliente(int idCliente) {
         this.idCliente = idCliente;
     }
 
@@ -48,14 +26,20 @@ public class Cliente extends Pessoa{
         return livrosEmprestados;
     }
 
+    // Método para emprestar livro
     public void emprestarLivro(Livro livro) {
         livrosEmprestados.add(livro);
+        livro.setStatus("Emprestado");
     }
 
+    // Método para devolver livro
     public void devolverLivro(Livro livro) {
         livrosEmprestados.remove(livro);
+        livro.setStatus("Disponível");
     }
 
+    // Sobrescrita do método para imprimir detalhes
+    @Override
     public void imprimirDetalhes() {
         super.imprimirDetalhes();
         System.out.println("ID do Cliente: " + idCliente);
