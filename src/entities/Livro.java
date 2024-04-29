55a2f696be6bf86910bbfb55f08d8dc1dd54c335
ThiagoCAraujo <133007228+ThiagoCAraujo@users.noreply.github.com>
2024-04-29 15:17:25 -0300

    Update Livro.java

--- src/entities/Livro.java
+++ src/entities/Livro.java
@@ -1,57 +1,54 @@
 package entities;
 
 public class Livro {
-	private String titulo;
-	private String autor;
-	private Integer idLivro;
-	private Boolean status;
-	
-	public Livro() {
-	}
+    public String titulo;
+    public String autor;
+    public int idLivro;
+    public String status;
 
-	public Livro(String titulo, String autor, Integer idLivro, Boolean status) {
-		this.titulo = titulo;
-		this.autor = autor;
-		this.idLivro = idLivro;
-		this.status = status;
-	}
+    // Construtor
+    public Livro(String titulo, String autor, int idLivro) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.idLivro = idLivro;
+        this.status = "Disponível";
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    // Getters e Setters
+    public String getTitulo() {
+        return titulo;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public String getAutor() {
-		return autor;
-	}
+    public String getAutor() {
+        return autor;
+    }
 
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 
-	public Integer getIdLivro() {
-		return idLivro;
-	}
+    public int getIdLivro() {
+        return idLivro;
+    }
 
-	public void setIdLivro(Integer idLivro) {
-		this.idLivro = idLivro;
-	}
+    public void setIdLivro(int idLivro) {
+        this.idLivro = idLivro;
+    }
 
-	public Boolean getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public void setStatus(Boolean status) {
-		this.status = status;
-	}
-	
-	public void exibirInformacoes() {
-		System.out.println("Titulo: " + titulo);
-        System.out.println("Autor: " + autor);
-        System.out.println("idLivro: " + idLivro);
-        System.out.println("Status: " + (status ? "Disponível" : "Emprestado"));
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    // Método para imprimir os detalhes do livro
+    public void imprimirDetalhes() {
+        System.out.println("- Título: " + titulo + ", Autor: " + autor + ", ID: " + idLivro);
+    }
 }
