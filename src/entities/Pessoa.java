14ead9339276c1d3c7376645359ef3c48e7a8ecb
ThiagoCAraujo <133007228+ThiagoCAraujo@users.noreply.github.com>
2024-04-29 15:16:55 -0300

    Update Pessoa.java

--- src/entities/Pessoa.java
+++ src/entities/Pessoa.java
@@ -1,45 +1,44 @@
 package entities;
 
 public class Pessoa {
-	private String nome;
-	private String endereco;
-	private String telefone;
-	
-	public Pessoa() {
-	}
+    private String nome;
+    private String endereco;
+    private String telefone;
 
-	public Pessoa(String nome, String endereco, String telefone) {
-		super();
-		this.nome = nome;
-		this.endereco = endereco;
-		this.telefone = telefone;
-	}
+    // Construtor
+    public Pessoa(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getEndereco() {
-		return endereco;
-	}
+    public String getEndereco() {
+        return endereco;
+    }
 
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
-	public String getTelefone() {
-		return telefone;
-	}
+    public String getTelefone() {
+        return telefone;
+    }
 
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	
-	public void imprimirDetalhes() {
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // Método para imprimir detalhes
+    public void imprimirDetalhes() {
         System.out.println("Nome: " + nome);
         System.out.println("Endereço: " + endereco);
         System.out.println("Telefone: " + telefone);
